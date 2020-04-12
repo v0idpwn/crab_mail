63488dbee160a9f5acf7bb06f3895cfda624dd9a
v0idpwn <v0idpwn@gmail.com>
2020-04-12 04:55:53 -0300

    steps 1 to 4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, App, HttpServer, Responder};
+use lettre::{ClientSecurity, SmtpClient, Transport};
+use lettre_email::EmailBuilder;
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    format!("OK")
+}
+
+#[post("/api/v1/send")]
+async fn send() -> impl Responder {
+    let email = EmailBuilder::new()
+        .to(("paleking@hallow.nest", "Pale King"))
+        .from("theknight@example.com")
+        .subject("Example email")
+        .html("We got html support")
+        .build()
+        .unwrap();
+
+    let mut mailer = SmtpClient::new(("localhost", 1025), ClientSecurity::None)
+        .unwrap()
+        .smtp_utf8(true)
+        .transport();
+
+    let result = mailer.send(email.into());
+    assert!(result.is_ok());
+    mailer.close();
+
+    format!("OK")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health).service(send))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
