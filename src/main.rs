8e637bfd0cc1bd8a58781eaca92c7789ab6cb899
v0idpwn <v0idpwn@gmail.com>
2020-04-12 15:23:15 -0300

    step 6

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use actix_web::{get, post, web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpServer, HttpResponse, Responder};
 use lettre::{ClientSecurity, SmtpClient, Transport};
 use lettre_email::EmailBuilder;
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 
 #[derive(Deserialize)]
 struct RequestBody {
@@ -11,31 +11,46 @@ struct RequestBody {
     html: String
 }
 
+#[derive(Serialize)]
+struct Response {
+    status: String,
+    message: String
+}
+
 #[get("/health")]
 async fn health() -> impl Responder {
     format!("OK")
 }
 
 #[post("/api/v1/send")]
-async fn send(body: web::Json<RequestBody>) -> impl Responder {
+async fn send(body: web::Json<RequestBody>) -> HttpResponse {
     let email = EmailBuilder::new()
         .to(body.to.clone())
         .from(body.from.clone())
         .subject(body.subject.clone())
         .html(body.html.clone())
-        .build()
-        .unwrap();
+        .build();
 
     let mut mailer = SmtpClient::new(("localhost", 1025), ClientSecurity::None)
         .unwrap()
         .smtp_utf8(true)
         .transport();
 
-    let result = mailer.send(email.into());
-    assert!(result.is_ok());
+    let response = 
+        match email {
+            Ok(e) => {
+              let result = mailer.send(e.into());
+              match result.is_ok() {
+                  true => ok_resp(),
+                  false => error_resp("error on sending routine")
+              }
+            },
+            Err(_) => error_resp("invalid params"),
+        };
+
     mailer.close();
 
-    format!("OK")
+    response
 }
 
 #[actix_rt::main]
@@ -44,4 +59,18 @@ async fn main() -> std::io::Result<()> {
         .bind("127.0.0.1:8080")?
         .run()
         .await
+}
+
+fn ok_resp() -> HttpResponse {
+    HttpResponse::Ok().json(Response{
+        status: "ok".to_string(),
+        message: "email sent".to_string()
+    })
+}
+
+fn error_resp(message: &str) -> HttpResponse {
+    HttpResponse::Ok().json(Response {
+        status: "error".to_string(),
+        message: message.to_string()
+    })
 }
